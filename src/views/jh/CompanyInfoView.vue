704b1d6f1efae9538590e6a3f8b321d9a34e015c
hyeon0714 <xodid6960@naver.com>
2024-05-09 16:30:33 +0900

    혀ㅑㄴ

--- src/views/jh/CompanyInfoView.vue
+++ src/views/jh/CompanyInfoView.vue
@@ -1,18 +1,60 @@
-<template>
-	<div></div>
+<template><!--업체 소개페이지-->
+	<div class="wrap">
+
+
+		<!--업체소개글-->
+		<div>
+			<div class="clearfix">
+				<div class="a">
+					<img class="img" src="../../assets/images/회사이미지.png" alt="">
+				</div>
+				<div>
+					<div>
+						<p class="companyname">업체명</p>
+						<button class="companybutton">업체 소개글 수정하기</button>
+					</div>
+					<p class="companypro">잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어잘낫어</p>
+				</div>
+			</div>
+		</div>
+		<!--클래스별 통계-->
+		<div class="clearfix">
+			<div class="cover1">
+				<img class="img2" src="../../assets/images/회사이미지.png" alt="">
+				<p class="classpro">클래스 소개글 샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라샬라</p>
+			</div>
+
+			<div class="cover2">
+				<div>
+					<p class="cover4 text1">이런점이 좋았어요</p>
+					<img class="img3 cover4" src="../../assets/images/회사이미지.png" alt="">
+					<p class="cover4 avg">4.88</p>
+					<p class="count1">99개의평점</p>
+				</div>
+				<div>
+					차트
+				</div>
+			</div>
+
+			
+
+		</div>
+
+	</div>
 </template>
 <script>
-import '../../assets/css/Initialization.css'
 
-	export default {
-		name: "CompanyInfoView",
-		components: {},
-		data() {
-			return {};
+import '@/assets/css/jh/jh.css'
+
+export default {
+	name: "CompanyInfoView",
+	components: {},
+	data() {
+		return {};
 	},
 	methods: {},
-	created(){}
-	};
+	created() { }
+};
 </script>
 
 <style></style>
