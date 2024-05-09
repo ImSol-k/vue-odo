02e81be756ac14cfb7ad15653058e765d5a0a1d0
ImSol-k <tiq0015@naver.com>
2024-05-09 14:25:45 +0900

    setting

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import CompanyJoinView from '../views/sr/CompanyJoinView.vue'
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
+    path: '/companyjoin',
+    name: 'companyjoin',
+    component: CompanyJoinView
   }
 ]
 
--- src/store/storage.js
+++ src/store/storage.js
@@ -4,7 +4,6 @@ import createPersistedState from 'vuex-persistedstate';
 export default createStore({
     state() {
         return {
-            //url베이스
             apiBaseUrl: "http://localhost:9090",
             //apiBaseUrl: "http://54.180.79.237:9090",
         };
