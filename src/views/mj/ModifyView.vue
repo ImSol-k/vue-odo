660be964b8130bb5b225631b64542be5e7d73d0f
m-ji97 <tv5035@naver.com>
2024-05-09 15:08:29 +0900

    mj

---
+++ src/views/mj/ModifyView.vue
@@ -0,0 +1,33 @@
+<template>
+	<div>
+		<div class="wrap">
+			<!-- container 부분 -->
+			<div class="MporderContainer">
+				<div class="MpLeft">
+					<!-- nav -->
+					<h2>내 활동</h2>
+				</div>
+				<!-- 오른쪽내용영역 -->
+				<div class="MpRight">
+
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+<script>
+import '@/assets/css/Initialization.css'
+import "@/assets/css/mj/modify.css"
+
+	export default {
+		name: "ModifyView",
+		components: {},
+		data() {
+			return {};
+	},
+	methods: {},
+	created(){}
+	};
+</script>
+
+<style></style>
