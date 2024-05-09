a6c42c96e8a8c166d86f4b311ff64c1543ec535b
ImSol-k <tiq0015@naver.com>
2024-05-09 19:51:05 +0900

    낼해야지

--- src/views/sr/CompanyJoinView.vue
+++ src/views/sr/CompanyJoinView.vue
@@ -1,5 +1,7 @@
 <template>
+	<AppHeader/>
 	<div class="wrap">
+		
 		<div class="companyJoinTitle">
 			<h1>사업자 가입</h1>
 			<div class="companyJoinImg">
@@ -12,44 +14,70 @@
 			</div><!--companyJoinImg-->
 			<div class="companyJoinInfo">
 				<div>
-					<label for="companyNo">사업자번호</label>
-					<input type="text" name="" id="companyNo">
-					<button>사업자 인증</button>
+					<!-- <label for="companyNo">사업자번호</label> -->
+					<input type="text" name="" id="companyNo" placeholder="사업자번호">
+					<input type="button" value="사업자 인증">
 				</div>
 				<div>
-					<label for="companyName">업체명</label>
-					<input type="text" name="" id="companyName">
+					<!-- <label for="companyName">업체명</label> -->
+					<input type="text" name="" id="companyName" placeholder="업체명">
 				</div>
 				<div>
 					
-					<label for="companyId">아이디</label>
-					<input type="text" name="" id="companyId">
-					<button>중복확인</button>
+					<!-- <label for="companyId">아이디</label> -->
+					<input type="text" name="" id="companyId" placeholder="아이디">
+					<input type="button" value="중복확인">
 					<p>✅사용 가능한 아이디입니다.</p>
+					<p>❌사용중인 아이디입니다.</p>
 				</div>
 				<div>
-					<label for="companyPass">비밀번호</label>
-					<input type="text" name="" id="companyPass">
+					<!-- <label for="companyPass">비밀번호</label> -->
+					<input type="text" name="" id="companyPass" placeholder="비밀번호">
 				</div>
 				<div>
-					<label for="companyPassChack">비밀번호확인</label>
-					<input type="text" name="" id="companyPassChack">
-					<p>❌비밀번호가 일치하지 않습니다.</p>
+					<!-- <label for="companyPassChack">비밀번호확인</label> -->
+					<input type="text" name="" id="companyPassChack" placeholder="비밀번호확인">
 					<p>✅비밀번호 일치</p>
+					<p>❌비밀번호가 일치하지 않습니다.</p>
+				</div>
+				<div class="companyAddress">
+					<input type="text" id="sample6_postcode" placeholder="우편번호">
+					<input type="button" onclick="sample6_execDaumPostcode()" value="주소검색">
+					<input type="text" id="sample6_address" placeholder="주소">
+					<input type="text" id="sample6_detailAddress" placeholder="상세주소">
+				</div>
+				<div>
+					<!-- <label for="companyPassChack">비밀번호확인</label> -->
+					<input type="text" name="" id="companyPassChack" placeholder="대표 핸드폰번호">
+					<input type="button" value="번호인증">
+					<div class="companyHpChack">
+						<input type="text" name="" id="companyPassChack" placeholder="인증번호">
+						<input type="button" value="확인">
+						<p>✅인증이 완료되었습니다.</p>
+						<p>❌인증번호가 일치하지 않습니다.</p>
+					</div>
+				</div>
+				<div class="companyJoinButton">
+					<button>취소</button>
+					<button>가입</button>
 				</div>
 			</div>
 				
 			
 		</div><!--companyJoinTitle-->
+		<!-- <AppFooter/> -->
 	</div>
 </template>
 <script>
 import '@/assets/css/Initialization.css'
 import '@/assets/css/sr/company.css'
+import "@/assets/css/hs/main.css"
+import AppHeader from "@/components/AppHeader.vue"
+
 
 	export default {
 		name: "CompanyJoinView",
-		components: {},
+		components: { AppHeader },
 		data() {
 			return {};
 	},
