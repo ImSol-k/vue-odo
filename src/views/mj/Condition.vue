3d9f9b9c1914db4418b16aa771bfe52349ccfba5
m-ji97 <tv5035@naver.com>
2024-05-09 17:40:44 +0900

    mj

---
+++ src/views/mj/Condition.vue
@@ -0,0 +1,14 @@
+<template>
+    <diV>
+        <div class="wrap">
+            <div class="MpLeft">
+                <!-- nav -->
+                <h2>내 활동</h2>
+            </div>
+            <div class="MpRight">
+                <h2>이용약관</h2>
+                
+            </div>
+        </div>
+    </diV>
+</template>
--- src/views/mj/ModifyView.vue
+++ src/views/mj/ModifyView.vue
@@ -5,11 +5,64 @@
 			<div class="MporderContainer">
 				<div class="MpLeft">
 					<!-- nav -->
-					<h2>내 활동</h2>
+					<ul>
+						<li><strong>내 활동</strong></li>
+						<li><router-link to="/mypage">결제 내역</router-link></li>
+						<li><router-link to="/mypage">위시리스트</router-link></li>
+						<li><router-link to="/mypage">쿠폰</router-link></li>
+						<li><router-link to="/mypage">후기</router-link></li>
+					</ul>
+					<hr>
+					<ul>
+						<li><strong>고객센터</strong></li>
+						<li><router-link to="/mypage">공지사항</router-link></li>
+						<li><router-link to="/mypage">약관 및 정책</router-link></li>
+					</ul>
+					<hr>
 				</div>
 				<!-- 오른쪽내용영역 -->
 				<div class="MpRight">
-
+					<h2>내 정보</h2>
+					<div class="ModifyImg">
+						<img src="@/assets/images/icon/man_icon.png" alt="">
+					</div>
+					<br>
+					<div class="form-blank">
+						<label for="group">닉네임</label>
+					</div>
+					<div>
+						<span id="fixid"> 이미지</span>
+					</div>
+					<div class="form-blank">
+						<label class="group">휴대폰번호</label>
+					</div>
+					<div>
+						<input type="hp" id="hp">
+					</div>
+					<div class="form-blank">
+						<label class="group">이메일</label>
+					</div>
+					<div>
+						<input type="e-mail" id="e-mail" v-model= required>
+					</div>
+					<div class="form-blank">
+						<label class="group">비밀번호</label>
+					</div>
+					<div>
+						<input type="password" id="password" v-model= required>
+					</div>
+					<div class="form-blank">
+						<label class="group">생년월일</label>
+					</div>
+					<div>
+						<input type="birth" id="birth" v-model= required>
+					</div>
+					<div class="form-blank">
+						<label class="group">성별</label>
+					</div>
+					<div>
+						<span id="fixid"> **</span>
+					</div>
 				</div>
 			</div>
 		</div>
