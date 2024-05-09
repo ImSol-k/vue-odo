c324b49d50c8a26535c4d9aec3d2bb599d8c1e57
ImSol-k <tiq0015@naver.com>
2024-05-09 15:02:04 +0900

    재현추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router'
+//소리
 import CompanyJoinView from '../views/sr/CompanyJoinView.vue'
+//재현
+import CompanyInfoView from '../views/jh/CompanyInfoView.vue'
 
 const routes = [
   {
     path: '/companyjoin',
     name: 'companyjoin',
     component: CompanyJoinView
+  },
+  {
+    path: '/companyinfo',
+    name: 'companyinfo',
+    component: CompanyInfoView
   }
 ]
 
