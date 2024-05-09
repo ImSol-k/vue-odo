3251d3caaddd5c82fe8c12fc2adc411bb06e359e
ImSol-k <tiq0015@naver.com>
2024-05-09 10:44:40 +0900

    setting

---
+++ src/views/sr/MainView.vue
@@ -0,0 +1,16 @@
+<template>
+	<div></div>
+</template>
+<script>
+	export default {
+		name: "",
+		components: {},
+		data() {
+			return {};
+	},
+	methods: {},
+	created(){}
+	};
+</script>
+
+<style></style>
