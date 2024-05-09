0a757412aac5fa78102de1ea49aed6250d94ef1c
rhs0402 <rhs0402@naver.com>
2024-05-09 18:03:49 +0900

    하석

--- src/views/hs/MainView.vue
+++ src/views/hs/MainView.vue
@@ -3,20 +3,20 @@
         <!-- //header -->
 		<div></div>
 
-		<!-- <AppFooter /> -->
+		<AppFooter />
         <!-- //footer -->
 </template>
 <script>
 import "@/assets/css/hs/main.css"
 
-// import AppFooter from "@/components/AppFooter.vue"
+import AppFooter from "@/components/AppFooter.vue"
 import AppHeader from "@/components/AppHeader.vue"
 
 	export default {
 		name: "MainView",
 		components: {
 			AppHeader,
-			// AppFooter
+			AppFooter
 		},
 		data() {
 			return {};
