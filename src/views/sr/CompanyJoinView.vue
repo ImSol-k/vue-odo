6287b76bb444275c41dae4ce3f69514f5e749947
ImSol-k <tiq0015@naver.com>
2024-05-09 14:44:51 +0900

    css추가

--- src/views/sr/CompanyJoinView.vue
+++ src/views/sr/CompanyJoinView.vue
@@ -1,7 +1,14 @@
 <template>
-	<div></div>
+	<div class="wrap">
+		<div class="companyJoinTitle">
+			<h1>사업자 가입</h1>
+		</div>
+	</div>
 </template>
 <script>
+import '@/assets/css/Initialization.css'
+import '@/assets/css/sr/company.css'
+
 	export default {
 		name: "CompanyJoinView",
 		components: {},
