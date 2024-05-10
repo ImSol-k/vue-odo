5cc793411727df9fe673b50d4a0a9caa015580ab
yss9358 <93y2s@naver.com>
2024-05-10 20:46:25 +0900

    로그인페이지까지함

--- src/views/ss/LoginPage.vue
+++ src/views/ss/LoginPage.vue
@@ -2,19 +2,20 @@
 
 	<AppHeader/>
 	<!-- header -->
-	
-	<div class="wrap">
-		<div class="login-join">
-			<span class="lj-txt">로그인</span>
-			<form class="loginform" action="" method="">
-				<input type="text" name="id" value="" placeholder="아이디를 입력하세요"><br>
-				<input type="password" name="password" value="" placeholder="비밀번호를 입력하세요"><br>
+	<div class="login-join">
+		<span class="lj-txt">로그인</span>
+		<div class="loginform">
+			<form  action="" method="">
+				<input type="text" name="id" value="" placeholder="아이디(이메일)"><br>
+				<input type="password" name="password" value="" placeholder="비밀번호"><br>
 				<button type="button">로그인</button>
 			</form>
-			
+			<span>다른 방식으로 로그인</span><br>
+			<img src="@/assets/images/icon/ss/kakao.png">
+			<img src="@/assets/images/icon/ss/na.png">
 		</div>
+		
 	</div>
-	<!-- wrap -->
 
 	<AppFooter/>
 	<!-- footer -->
