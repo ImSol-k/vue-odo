b9780d9a6cbc2f463e613ee5b55a7deebef212ed
ImSol-k <tiq0015@naver.com>
2024-05-10 14:54:45 +0900

    승수

--- src/views/sr/ClassAddView.vue
+++ src/views/sr/ClassAddView.vue
@@ -18,25 +18,22 @@
           >
         </div>
         <div class="classAddBox">
-          <div>
             <img src="@/assets/images/icon/ss/kakao.png" alt="" />
-            <div class="classAdd">
+            <div class="classAddImg">
               <p>대표이미지</p>
               <input type="file" name="" id="" />
+              <select name="" id="" class="addPageSelectClassMemory">
+                <option value="" disabled selected>기존클래스 불러오기</option>
+                <option value="" v-for="i in 5" :key="i">
+                  기존에 사용했던 클래스명 불러오기
+                </option>
+              </select>
             </div>
-
+          <div>
             <select name="" id="" class="addPageSelectClass">
               <option value="" disabled selected>관련 정규클래스 선택</option>
               <option value="" v-for="i in 5" :key="i">
                 정규클래스 {{ i }}번
-              </option>
-            </select>
-          </div>
-          <div>
-            <select name="" id="" class="addPageSelectClassMemory">
-              <option value="" disabled selected>기존클래스 불러오기</option>
-              <option value="" v-for="i in 5" :key="i">
-                기존에 사용했던 클래스명 불러오기
               </option>
             </select>
           </div>
