61ed97d873bfe7acd84ea378bb33bc2ec6801a1d
hyeon0714 <xodid6960@naver.com>
2024-05-10 10:00:08 +0900

    재현 차트추가

---
+++ src/views/jh/CompanyInfoModify.vue
@@ -0,0 +1,38 @@
+<template><!--업체 소개페이지-->
+	<div class="wrap">
+
+		<!--헤더-->
+
+		<AppHeader/>
+
+		
+		
+
+		<AppFooter/>
+
+	</div>
+</template>
+<script>
+import AppHeader from "@/components/AppHeader.vue"
+import AppFooter from "@/components/AppFooter.vue"
+import '@/assets/css/jh/jh.css'
+
+
+
+export default {
+	components:{
+		AppHeader,
+		AppFooter
+	},
+	data:{},
+	
+	methods: {
+		
+	},
+	created(){
+		
+	}
+};
+</script>
+
+<style></style>
