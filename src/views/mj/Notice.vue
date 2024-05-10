9e552a5ee675327fb1d7ba861ba6d04b8b683469
m-ji97 <tv5035@naver.com>
2024-05-10 15:06:15 +0900

    mj

---
+++ src/views/mj/Notice.vue
@@ -0,0 +1,30 @@
+<template>
+	<div>
+		<!-- AppHeader -->
+		<AppHeader />
+		<div class="wrap">
+		</div>
+    </div>
+		<!-- 푸터 -->
+		<AppFooter/>
+</template>
+<script>
+import '@/assets/css/Initialization.css'
+import AppFooter from '@/components/AppFooter.vue';
+import AppHeader from '@/components/AppHeader.vue';
+
+	export default {
+		name: "ModifyView",
+		components: {
+			AppHeader,
+			AppFooter
+		},
+		data() {
+			return {};
+	},
+	methods: {},
+	created(){}
+	};
+</script>
+
+<style></style>
