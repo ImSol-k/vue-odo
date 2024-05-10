2fa50a51fea0b983eb30b076920658972643099a
rhs0402 <rhs0402@naver.com>
2024-05-10 18:08:33 +0900

    하석

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -10,7 +10,7 @@
         <ul>
             <li @click="toggleSubMenu1">
                 <router-link to="#">쿠킹</router-link>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen1 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[1] }">
                     <li><a href="#">한식</a></li>
                     <li><a href="#">일식</a></li>
                     <li><a href="#">중식</a></li>
@@ -19,7 +19,7 @@
             </li>
             <li @click="toggleSubMenu2">
                 <router-link to="#">베이킹</router-link>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen2 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[2] }">
                     <li><a href="#">제과</a></li>
                     <li><a href="#">제빵</a></li>
                     <li><a href="#">쇼콜라</a></li>
@@ -28,14 +28,14 @@
             </li>
             <li>
                 <a href="#" @click="toggleSubMenu3">음료</a>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen3 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[3] }">
                     <li><a href="#">주류</a></li>
                     <li><a href="#">바리스타</a></li>
                 </ul>
             </li>
             <li>
                 <a href="#" @click="toggleSubMenu4">뷰티</a>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen4 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[4] }">
                     <li><a href="#">네일</a></li>
                     <li><a href="#">미용</a></li>
                     <li><a href="#">메이크업</a></li>
@@ -44,7 +44,7 @@
             </li>
             <li>
                 <a href="#" @click="toggleSubMenu5">공예</a>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen5 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[5] }">
                     <li><a href="#">비즈</a></li>
                     <li><a href="#">뜨게질</a></li>
                     <li><a href="#">자수</a></li>
@@ -54,7 +54,7 @@
             </li>
             <li>
                 <a href="#" @click="toggleSubMenu6">스포츠</a>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen6 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[6] }">
                     <li><a href="#">수영</a></li>
                     <li><a href="#">축구</a></li>
                     <li><a href="#">야구</a></li>
@@ -65,7 +65,7 @@
             </li>
             <li>
                 <a href="#" @click="toggleSubMenu7">심리/상담</a>
-                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen7 }">
+                <ul class="subMenuList" :class="{ 'show': isSubMenuOpen[7] }">
                     <li><a href="#">부부상담</a></li>
                     <li><a href="#">가정상담</a></li>
                     <li><a href="#">심리상담</a></li>
@@ -119,47 +119,45 @@ export default {
     data() {
         return {
             isMenuOpen: false,
-            isSubMenuOpen1: false,
-            isSubMenuOpen2: false,
-            isSubMenuOpen3: false,
-            isSubMenuOpen4: false,
-            isSubMenuOpen5: false,
-            isSubMenuOpen6: false,
-            isSubMenuOpen7: false,
+            isSubMenuOpen:  {
+                1: false,
+                2: false,
+                3: false,
+                4: false,
+                5: false,
+                6: false,
+                7: false,
+            },
         };
     },
     methods: {
-        
+
         toggleMenu() {
             this.isMenuOpen = !this.isMenuOpen;
-            this.isSubMenuOpen1 = false;
-            this.isSubMenuOpen2 = false;
-            this.isSubMenuOpen3 = false;
-            this.isSubMenuOpen4 = false;
-            this.isSubMenuOpen5 = false;
-            this.isSubMenuOpen6 = false;
-            this.isSubMenuOpen7 = false;
+            for(let i=1; i<8; i++) {
+                this.isSubMenuOpen[i] = false;
+            }
         },
-        toggleSubMenu1(){
-            this.isSubMenuOpen1 = !this.isSubMenuOpen1;
+        toggleSubMenu1() {
+            this.isSubMenuOpen[1] = !this.isSubMenuOpen[1];
         },
-        toggleSubMenu2(){
-            this.isSubMenuOpen2 = !this.isSubMenuOpen2;
+        toggleSubMenu2() {
+            this.isSubMenuOpen[2] = !this.isSubMenuOpen[2];
         },
-        toggleSubMenu3(){
-            this.isSubMenuOpen3 = !this.isSubMenuOpen3;
+        toggleSubMenu3() {
+            this.isSubMenuOpen[3] = !this.isSubMenuOpen[3];
         },
-        toggleSubMenu4(){
-            this.isSubMenuOpen4 = !this.isSubMenuOpen4;
+        toggleSubMenu4() {
+            this.isSubMenuOpen[4] = !this.isSubMenuOpen[4];
         },
-        toggleSubMenu5(){
-            this.isSubMenuOpen5 = !this.isSubMenuOpen5;
+        toggleSubMenu5() {
+            this.isSubMenuOpen[5] = !this.isSubMenuOpen[5];
         },
-        toggleSubMenu6(){
-            this.isSubMenuOpen6 = !this.isSubMenuOpen6;
+        toggleSubMenu6() {
+            this.isSubMenuOpen[6] = !this.isSubMenuOpen[6];
         },
-        toggleSubMenu7(){
-            this.isSubMenuOpen7 = !this.isSubMenuOpen7;
+        toggleSubMenu7() {
+            this.isSubMenuOpen[7] = !this.isSubMenuOpen[7];
         },
     },
     created() { }
