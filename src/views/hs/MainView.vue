4b4f3b0b82239155b36f30b606361c76bdc973d2
ryuhaseok <rhs0402@naver.com>
2024-05-11 18:56:09 +0900

    하석

--- src/views/hs/MainView.vue
+++ src/views/hs/MainView.vue
@@ -1,30 +1,129 @@
 <template>
 	<AppHeader />
 	<!-- //header -->
-	<section class="main-slide"></section>
-	<section class="free-best"></section>
-	<div class="banner"></div>
-	<section class="pay-best"></section>
+	<div class="inner">
+		<section class="main-slide">
+			<Carousel :autoplay="2500" :wrap-around="true">
+				<Slide v-for="slide in slides" :key="slide">
+					<div class="carousel__item">
+						<img class="slideImg" src="{{slide}}" alt="">
+					</div>
+				</Slide>
+
+				<template #addons>
+					<Pagination />
+					<Navigation />
+				</template>
+			</Carousel>
+		</section>
+		<ul class="category-icons">
+			<li><router-link to=""><img src="@/assets/images/하트.png" alt="">
+					<p>쿠킹</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>베이킹</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>음료</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>뷰티</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>공예</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>스포츠</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>심리/상담</p>
+				</router-link></li>
+			<li><router-link to=""><img src="" alt="">
+					<p>IT</p>
+				</router-link></li>
+		</ul>
+		<section class="free-best">
+			<div class="title-box">
+				<h3>무료 베스트 클래스
+					<img src="../../assets/images/하트2.png" alt="">
+				</h3>
+				<router-link to="#">전체보기</router-link>
+			</div>
+			<ul class="classList">
+				<li><router-link to="">
+					<div class="img-box">
+						<img src="" alt="">
+						<img src="" alt="">
+					</div>
+					<p class="location">강남/서초</p>
+					<p class="classTitle">소풍을 즐기는 새로운 방법, 루하루 티크닉 세트[SQN..</p>
+					<div class="review-box">
+						<span>후기 123</span>
+					</div>
+					<p class="class-price">무료</p>
+				</router-link></li>
+			</ul>
+		</section>
+		<div class="banner"></div>
+		<section class="pay-best"></section>
+	</div>
 	<AppFooter />
 	<!-- //footer -->
 </template>
 <script>
 import "@/assets/css/hs/main.css"
+import "@/assets/css/hs/mainView.css"
+
+import { defineComponent } from 'vue'
+import { Carousel, Navigation, Pagination, Slide } from 'vue3-carousel'
+import 'vue3-carousel/dist/carousel.css'
+
+import main_slide_1 from "@/assets/images/hs/main_slide_1.jpg"
+import main_slide_2 from "@/assets/images/hs/main_slide_2.jpg"
+import main_slide_3 from "@/assets/images/hs/main_slide_3.jpg"
+import main_slide_4 from "@/assets/images/hs/main_slide_4.jpg"
+import main_slide_5 from "@/assets/images/hs/main_slide_5.jpg"
 
 import AppFooter from "@/components/AppFooter.vue"
 import AppHeader from "@/components/AppHeader.vue"
 
-
-	export default {
-		name: "MainView",
-		components: {
-			AppHeader,
-			AppFooter,
-		},
-		data() {
-			return {};
+export default defineComponent({
+	name: "MainView",
+	components: {
+		AppHeader,
+		AppFooter,
+		Carousel,
+		Slide,
+		Pagination,
+		Navigation,
+	},
+	data() {
+		return {
+			slides: [main_slide_1, main_slide_2, main_slide_3, main_slide_4, main_slide_5],
+		};
 	},
 	methods: {},
-	created(){}
-	};
+	created() { }
+});
 </script>
+<style>
+.carousel__item {
+	min-height: 380px;
+	width: 100%;
+	background-color: green;
+	color: var(--vc-clr-white);
+	font-size: 20px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.carousel__slide {
+	padding: 10px;
+}
+
+.carousel__prev,
+.carousel__next {
+	box-sizing: content-box;
+	color: #fff;
+}</style>
