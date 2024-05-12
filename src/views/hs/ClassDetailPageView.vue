b439456a9df79b35a3ae48099a814ddbe07d56b5
ryuhaseok <rhs0402@naver.com>
2024-05-12 19:15:36 +0900

    ㅎㅅ

--- src/views/hs/ClassDetailPageView.vue
+++ src/views/hs/ClassDetailPageView.vue
@@ -2,12 +2,48 @@
     <AppHeader />
 	<!-- //header -->
 
-	<div></div>
+	<div class="inner">
+		<div class="headSection">
+			<img src="" alt="">
+
+			<div class="headInfoBox">
+				<h2>소풍을 즐기는 새로운 방법, 루하루 티크닉 세트 [SQNC 055]</h2>
+				<p>30,000원</p>
+
+				<div class="companySection">
+					<router-link to="">
+						<img src="" alt="">
+					</router-link>
+					<router-link to="" class="nameBox">
+						<p></p>
+						<p></p>
+					</router-link>
+					<router-link to="" class="company-like-btn">
+						<img src="" alt="">
+					</router-link>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //inner -->
+
+	<div class="floatingActionBar">
+		<div class="action-box">
+			<router-link to="#" class="like-actBtn">
+				<img src="../../assets/images/하트.png" alt="">
+				<p>13</p>
+			</router-link>
+			<button>참여하기</button>
+		</div>
+	</div>
+	<!-- //floatingActionBar -->
 
     <AppFooter />
 	<!-- //footer -->
 </template>
 <script>
+import "@/assets/css/hs/classDetailPage.css"
+
 import AppFooter from "@/components/AppFooter.vue"
 import AppHeader from "@/components/AppHeader.vue"
 
