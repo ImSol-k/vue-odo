84877f52050d2aba2ff61dcd826179813a11644f
m-ji97 <tv5035@naver.com>
2024-05-13 17:48:42 +0900

    mj

--- src/views/mj/ModifyView.vue
+++ src/views/mj/ModifyView.vue
@@ -7,21 +7,10 @@
 			<!-- container 부분 -->
 			<div class="MporderContainer">
 				<div class="MpLeft">
+					
 					<!-- nav -->
-					<ul>
-						<li><strong>내 활동</strong></li>
-						<li><router-link to="">결제 내역</router-link></li>
-						<li><router-link to="">위시리스트</router-link></li>
-						<li><router-link to="">쿠폰</router-link></li>
-						<li><router-link to="">후기</router-link></li>
-					</ul>
-					<hr>
-					<ul>
-						<li><strong>고객센터</strong></li>
-						<li><router-link to="">공지사항</router-link></li>
-						<li><router-link to="">약관 및 정책</router-link></li>
-					</ul>
-					<hr>
+					<MyPageSide/>
+
 				</div>
 				<!-- 오른쪽내용영역 -->
 				<div class="MpRight" style="overflow: hidden;">
@@ -96,12 +85,14 @@ import '@/assets/css/Initialization.css'
 import "@/assets/css/mj/modify.css"
 import AppFooter from '@/components/AppFooter.vue';
 import AppHeader from '@/components/AppHeader.vue';
+import MyPageSide from '@/components/MyPageSide.vue';
 
 	export default {
 		name: "ModifyView",
 		components: {
 			AppHeader,
-			AppFooter
+			AppFooter,
+			MyPageSide
 		},
 		data() {
 			return {};
