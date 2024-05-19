81886706cfddeff839d858b431c95686a942f0c3
ryuhaseok <rhs0402@naver.com>
2024-05-19 18:34:16 +0900

    hs

---
+++ src/views/hs/SearchResultPageView.vue
@@ -0,0 +1,14 @@
+<template>
+    <div></div>
+    </template>
+    
+    <script>
+    export default {
+    name: "SearchResultPageView",
+    components: {},
+    data() {
+    return {};
+    },
+    methods: {},
+    };
+    </script>
