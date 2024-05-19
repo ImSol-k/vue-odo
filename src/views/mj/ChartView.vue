40237f6bb6be40a819b803f838191d7cd88b1914
m-ji97 <tv5035@naver.com>
2024-05-19 18:33:24 +0900

    mj

--- src/views/mj/ChartView.vue
+++ src/views/mj/ChartView.vue
@@ -1,104 +1,69 @@
 <template>
-	<div>
-		<!-- AppHeader -->
-		<AppHeader />
-		<div class="wrap">
-			
-			<!-- container 부분 -->
-			<div class="MporderContainer">
-				<div class="MpLeft">
-					
-					<!-- nav -->
-					<MyPageSide/>
+  <AppHeader />
+  <div class="wrap">
+    <div class="clearfix">
+      <AppMenu />
+      <div class="chartArea">
+        <!-- line chart -->
+        <h2>매출</h2>
+        <div>
+          <canvas id="LineChart" width="400" height="400"></canvas>
+        </div>
+      </div>
 
-				</div>
-				<!-- 오른쪽내용영역 -->
-				<div class="MpRight" style="overflow: hidden;">
-					<h2>내 정보</h2>
-					<div class="container Right">
-						<button>저장</button>
-					</div>
-					
-					<div class="box">
-						<img class="profile" src="@/assets/images/하트.png">
-					</div>
-					<div id="p-box">
-						<input type="file" name="" id="picture">
-					</div>
-					<div class="area">
-						<div class="form-blank">
-							<label class="group">닉네임</label>
-						</div>
-						<div>
-							<input type="text" id="nickname" class="form_w100" placeholder="이미지" v-model=required>
-						</div>
-					</div>
-					<div class="area">
-						<div class="form-blank">
-							<label class="group">휴대폰번호</label>
-						</div>
-						<div>
-							<input type="text" id="hp" class="form_w100" disabled placeholder ="010-3333-3333" v-model="required">
-						</div>
-					</div>
-					<div class="area">
-						<div class="form-blank">
-							<label class="group">이메일</label>
-						</div>
-						<div>
-							<input type="text" id="e-mail" class="form_w100" disabled placeholder ="admin@admin.com" v-model="required">
-						</div>
-					</div>
-					<div class="area">
-						<div class="form-blank">
-							<label class="group">비밀번호</label>
-						</div>
-						<div>
-							<input type="password" id="password" class="form_w100" disabled placeholder ="****" v-model="required">
-						</div>
-					</div>
-					<div class="area">
-						<div class="form-blank">
-							<label class="group">생년월일</label>
-						</div>
-						<div>
-							<input type="text" id="birth" class="form_w100" disabled placeholder ="981217" v-model="required">
-						</div>
-					</div>
-					<div>
-						<div class="form-blank">
-							<label class="group">성별</label>
-						</div>
-						<div>
-							<input type="text" id="gender" class="form_w100" disabled placeholder ="**" v-model="required">
-						</div>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-		<!-- 푸터 -->
-		<AppFooter/>
+    </div>
+  </div>
+  <AppFooter />
 </template>
 <script>
-import '@/assets/css/Initialization.css'
-// import "@/assets/css/mj/modify.css"
-import AppFooter from '@/components/AppFooter.vue';
-import AppHeader from '@/components/AppHeader.vue';
-import MyPageSide from '@/components/MyPageSide.vue';
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+import AppMenu from "@/components/CompanyMenu.vue";
+import "@/assets/css/mj/chart.css";
+import { Chart, registerables } from 'chart.js';
+Chart.register(...registerables);
 
-	export default {
-		name: "ChartView",
+export default {
+		name: "ModifyView",
 		components: {
 			AppHeader,
 			AppFooter,
-			MyPageSide
+			AppMenu
 		},
 		data() {
 			return {};
 	},
 	methods: {},
-	created(){}
+	created(){},
+  mounted() {
+    const labels = [
+      'January',
+      'February',
+      'March',
+      'April',
+      'May',
+      'June',
+    ];
+    const data = {
+      labels: labels,
+      datasets: [{
+        label: 'My First Dataset',
+        data: [65, 59, 80, 81, 56, 55, 40],
+        fill: false,
+        borderColor: 'rgb(75, 192, 192)',
+        tension: 0.1
+      }]
+    };
+    const config = {
+      type: 'line',
+      data: data,
+    };
+    new Chart(
+      document.getElementById('LineChart'),
+      config
+    );
+  },
+
 	};
 </script>
 
