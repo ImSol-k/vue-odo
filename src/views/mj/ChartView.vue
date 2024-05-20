ec3de326117d5319be876a5ee0e082ca11297c97
m-ji97 <tv5035@naver.com>
2024-05-20 10:03:05 +0900

    mj

--- src/views/mj/ChartView.vue
+++ src/views/mj/ChartView.vue
@@ -37,22 +37,30 @@ export default {
 	created(){},
   mounted() {
     const labels = [
-      'January',
-      'February',
-      'March',
-      'April',
-      'May',
-      'June',
+      '24년 1월',
+      '2월',
+      '3월',
+      '4월',
+      '5월',
+      '24년 6월',
     ];
     const data = {
       labels: labels,
       datasets: [{
-        label: 'My First Dataset',
+        label: '원데이',
         data: [65, 59, 80, 81, 56, 55, 40],
         fill: false,
         borderColor: 'rgb(75, 192, 192)',
-        tension: 0.1
-      }]
+        tension: 0.2
+      },
+      {
+        label: '정규',
+        data:[60, 65, 70, 65, 75, 80],
+        fill:false,
+        borderColor: 'rgb(30, 200, 120)',
+        tension: 0.2
+      }
+    ],
     };
     const config = {
       type: 'line',
