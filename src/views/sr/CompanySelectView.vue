76e40020377cbd7e17d24de79a03c29029a97889
ImSol-k <tiq0015@naver.com>
2024-05-20 16:25:37 +0900

    2024/05/20 소리

---
+++ src/views/sr/CompanySelectView.vue
@@ -0,0 +1,31 @@
+<template>
+    <AppHeader/>
+	<div class="wrap">
+        <div class="companySelectBox">
+            
+            <p>환영합니다</p>
+            <ul>
+                <li><router-link to="/">로그인</router-link></li>
+                <li><router-link to="/">회원가입</router-link></li>
+            </ul>
+        </div>
+        
+    </div>
+    <AppFooter/>
+</template>
+<script>
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
+
+	export default {
+		name: "CompanySelectView",
+		components: { AppHeader,AppFooter },
+		data() {
+			return {};
+	},
+	methods: {},
+	created(){}
+	};
+</script>
+
+<style></style>
--- src/views/ss/LoginPage.vue
+++ src/views/ss/LoginPage.vue
@@ -1,79 +1,102 @@
 <template>
+  <AppHeader />
+  <!-- header -->
 
-	<AppHeader/>
-	<!-- header -->
-
-	<div class="login-join">
-		<span class="lj-txt">로그인</span>
-		<div class="lj-form">
-			<form v-on:submit.prevent="login">
-				<input type="text" name="id" v-model="loginVo.id" placeholder="아이디(이메일)">
-				<!-- <img v-if="isPass" class="lj-passImg" :src="pass1" v-on:click.prevent="changeInputImg">
+  <div class="login-join" v-if="isUser">
+    <span class="lj-txt">로그인</span>
+    <div class="lj-form">
+      <form v-on:submit.prevent="login">
+        <input
+          type="text"
+          name="id"
+          v-model="loginVo.id"
+          placeholder="아이디(이메일)"
+        />
+        <!-- <img v-if="isPass" class="lj-passImg" :src="pass1" v-on:click.prevent="changeInputImg">
 				<img v-else class="lj-passImg" :src="pass2" v-on:click.prevent="changeInputImg"> -->
-				<input type="password" name="password" v-model="loginVo.password" placeholder="비밀번호">
-				<button class="login-btn" type="submit">로그인</button>
-			</form>
-			<!-- form -->
-			<span class="other-login">다른 방식으로 로그인</span>
-			<img src="@/assets/images/icon/ss/kakao.png" >
-			<img src="@/assets/images/icon/ss/na.png">
-		</div>
-		<!-- lj-form -->
-	</div>
-	<!-- login-join -->
+        <input
+          type="password"
+          name="password"
+          v-model="loginVo.password"
+          placeholder="비밀번호"
+        />
+        <button class="login-btn" type="submit">로그인</button>
+      </form>
+      <!-- form -->
+      <span class="other-login">다른 방식으로 로그인</span>
+      <img src="@/assets/images/icon/ss/kakao.png" />
+      <img src="@/assets/images/icon/ss/na.png" />
+    </div>
+    <!-- lj-form -->
+  </div>
+  <!-- login-join -->
 
-	<AppFooter/>
-	<!-- footer -->
-	
+  <div class="login-join" v-else>
+    <span class="lj-txt">사업자 로그인</span>
+    <div class="lj-form">
+      <form v-on:submit.prevent="login">
+        <input
+          type="text"
+          name="id"
+          v-model="loginVo.id"
+          placeholder="아이디"
+        />
+        <!-- <img v-if="isPass" class="lj-passImg" :src="pass1" v-on:click.prevent="changeInputImg">
+				<img v-else class="lj-passImg" :src="pass2" v-on:click.prevent="changeInputImg"> -->
+        <input
+          type="password"
+          name="password"
+          v-model="loginVo.password"
+          placeholder="비밀번호"
+        />
+        <button class="login-btn" type="submit">로그인</button>
+      </form>
+    </div>
+    <!-- lj-form -->
+  </div>
+
+  <AppFooter />
+  <!-- footer -->
 </template>
 
-
-
-
 <script>
-import '@/assets/css/Initialization.css';
-import '@/assets/css/ss/login-join.css';
-import AppHeader from '@/components/AppHeader.vue';
-import AppFooter from '@/components/AppFooter.vue';
-
+import "@/assets/css/Initialization.css";
+import "@/assets/css/ss/login-join.css";
+import AppHeader from "@/components/AppHeader.vue";
+import AppFooter from "@/components/AppFooter.vue";
 
 export default {
-	name: "LoginPage",
-	components: {
-		AppHeader,
-		AppFooter
-	},
-	data() {
-		return {
-			loginVo :{
-				id: '',
-				password: '',
-			},
-			pass1 : require('@/assets/images/icon/ss/pass1.png'),
-			pass2 : require('@/assets/images/icon/ss/pass2.png'),
-			isPass : true
-		};
-	},
-	methods: {
-		// 로그인
-		login(){
-			console.log('로그인');
-			console.log(this.loginVo);
-		},
+  name: "LoginPage",
+  components: {
+    AppHeader,
+    AppFooter,
+  },
+  data() {
+    return {
+      loginVo: {
+        id: "",
+        password: "",
+      },
+      pass1: require("@/assets/images/icon/ss/pass1.png"),
+      pass2: require("@/assets/images/icon/ss/pass2.png"),
+      isPass: true,
+      isUser: false,
+    };
+  },
+  methods: {
+    // 로그인
+    login() {
+      console.log("로그인");
+      console.log(this.loginVo);
+    },
 
-		// 비밀번호인풋창 바꾸기
-		changeInputImg(){
-			this.isPass = !this.isPass;
-		}
-		
-	},
-	created(){}
+    // 비밀번호인풋창 바꾸기
+    changeInputImg() {
+      this.isPass = !this.isPass;
+    },
+  },
+  created() {},
 };
 </script>
 
-
-
-
-
-<style>
-</style>
+<style></style>
