8d93ad18d13273eef51b80108e030bb17f271ce0
hyeon0714 <xodid6960@naver.com>
2024-05-24 17:58:30 +0900

    gg

--- src/views/jh/WishListClass.vue
+++ src/views/jh/WishListClass.vue
@@ -40,39 +40,44 @@
               <RouterLink to="wishlistcompany">업체</RouterLink>
             </span>
           </div>
+
+          <v-if>
+
+          </v-if>
           <!-- mymy-nav -->
           <!--위시 리스트 없을때-->
-          <!--
-                    <div class="cover31">
-                        
-                        <div>
-                            <img class="img30" src="../../assets/images/하트.png" alt="">
-                        </div>
-                        <div class="cover30">
-                            <p>아직 찜한 클래스가 없어요</p>
-                            <p>관심있는 클래스를 선택해보세요</p>
-                        </div>
-                        
+          <!---->
+          <div class="cover31" v-if="this.b <= 0">
+
+            <div>
+              <img class="img30" src="../../assets/images/하트.png" alt="">
+            </div>
+            <div class="cover30">
+              <p>아직 찜한 클래스가 없어요</p>
+              <p>관심있는 클래스를 선택해보세요</p>
+            </div>
 
 
-                    </div>
-                    -->
-          <div class="wishlistClassBox">
+
+          </div>
+
+          <div class="wishlistClassBox" v-else>
             <!--위시리스트 있을때-->
             <div class="wishClassBox" v-for="(a, i) in b" :key="i">
               <div class="whsilistImg">
-                <img class="" v-bind:src="getImageSrc(a.classImage)"  alt="" @click="goDetailPage" />
-                <img v-on:click.prevent="modalUp" style="cursor: pointer" class=""
+                <img v-if="a.className != null" class="" v-bind:src="getImageSrc(a.classImage)" alt="" @click="goDetailPage" />
+                <img v-else class="" src="../../assets/images/class/이미지없음.jpg" alt="" @click="goDetailPage" />
+                <img v-on:click.prevent="modalUp(a.no)" style="cursor: pointer" class=""
                   src="data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cg clip-path='url(%23clip0_2517_457)'%3E %3Cpath d='M15.4448 27.0641L15.4368 27.0593L15.4064 27.0433C14.775 26.6998 14.1549 26.3359 13.5472 25.9521C12.0981 25.0404 10.7174 24.0243 9.41595 22.9121C6.47195 20.3729 3.19995 16.5633 3.19995 12.0001C3.20009 10.5112 3.66179 9.05903 4.52147 7.84344C5.38115 6.62785 6.59654 5.70866 8.00027 5.21244C9.40401 4.71622 10.9271 4.66737 12.3597 5.07263C13.7924 5.47788 15.0641 6.31729 16 7.4753C16.9358 6.31729 18.2075 5.47788 19.6402 5.07263C21.0728 4.66737 22.5959 4.71622 23.9996 5.21244C25.4034 5.70866 26.6187 6.62785 27.4784 7.84344C28.3381 9.05903 28.7998 10.5112 28.8 12.0001C28.8 16.5633 25.5296 20.3729 22.584 22.9121C20.7349 24.492 18.7274 25.8764 16.5936 27.0433L16.5632 27.0593L16.5552 27.0641H16.552C16.3819 27.1542 16.1925 27.2014 16.0001 27.2017C15.8078 27.2019 15.6182 27.1553 15.448 27.0657L15.4448 27.0641Z' fill='%23FD2748'/%3E %3C/g%3E %3Cdefs%3E %3CclipPath id='clip0_2517_457'%3E %3Crect width='32' height='32' fill='white'/%3E %3C/clipPath%3E %3C/defs%3E %3C/svg%3E"
                   alt="" />
               </div>
               <div class="wishClassContent">
-                <p class="">{{a.classAddress}}</p>
-                <p class="">{{a.className}}</p>
+                <p class="">{{ a.classAddress }}</p>
+                <p class="">{{ a.className }}</p>
                 <p class="">
-                  {{a.classIntro}}
+                  {{ a.classIntro }}
                 </p>
-                <p class="">{{a.classPrice}}</p>
+                <p class="">{{ a.classPrice }}</p>
               </div>
             </div>
           </div>
@@ -135,8 +140,8 @@
         <p>삭제 하시겠습니까?</p>
       </div>
       <div class="modal-b">
-        <button>취소</button>
-        <button v-on:click="close">삭제</button>
+        <button v-on:click="close">취소</button>
+        <button v-on:click="del">삭제</button>
       </div>
     </div>
   </div>
@@ -164,7 +169,8 @@ export default {
   data() {
     return {
       a: [1, 2, 3, 4, 5, 6],
-      b: []
+      b: [],
+      c: ''
     };
   },
 
@@ -173,9 +179,10 @@ export default {
       // 경로를 직접 구성합니다.
       return require(`@/assets/images/class/${imageName}.jpg`);
     },
-    modalUp() {
-      console.log("업");
+    modalUp(a) {
       let viewModal = document.querySelector("#newModal");
+
+      this.c = a;
 
       viewModal.style.display = "block";
     },
@@ -197,14 +204,27 @@ export default {
         url: `${this.$store.state.apiBaseUrl}/odo/wishclass`,
         headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": "Bearer " + this.$store.state.token }, //전송타입
         //params: guestbookVo, //get방식 파라미터로 값이 전달
-        data: this.$store.state.authUser.userNo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+        //data: this.$store.state.authUser.userNo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
         responseType: 'json' //수신타입
       }).then(response => {
         console.log(response); //수신데이타
-        
+        console.log(response.data.result);
         this.b = response.data.apiData
-        console.log(this.b);
-        console.log(this.b[0].classImage);
+      }).catch(error => {
+        console.log(error);
+      });
+    },
+    del(){
+      axios({
+        method: 'delete', // put, post, delete 
+        url: `${this.$store.state.apiBaseUrl}/odo/wishclass`,
+        headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": "Bearer " + this.$store.state.token }, //전송타입
+        //params: guestbookVo, //get방식 파라미터로 값이 전달
+        data: this.c, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+        responseType: 'json' //수신타입
+      }).then(response => {
+        console.log(response); //수신데이타
+        window.location.reload(true);
       }).catch(error => {
         console.log(error);
       });
@@ -212,7 +232,7 @@ export default {
   },
   created() {
     this.listup();
-   },
+  },
 };
 </script>
 
--- src/views/jh/WishListCompany.vue
+++ src/views/jh/WishListCompany.vue
@@ -3,7 +3,7 @@
   <AppHeader />
   <div class="wrap">
 
-    <MyPageHeader/>
+    <MyPageHeader />
     <!-- <div class="mymy-header clearfix">
       <img
         src="@/assets/images/icon/ss/default-profile.png"
@@ -41,40 +41,33 @@
           </div>
           <!-- mymy-nav -->
           <!--위시 리스트 없을때-->
-          <!--
-                    <div class="cover31">
-                        
-                        <div>
-                            <img class="img30" src="../../assets/images/하트.png" alt="">
-                        </div>
-                        <div class="cover30">
-                            <p>아직 찜한 업체가 없어요</p>
-                            <p>관심있는 업체를 선택해보세요</p>
-                        </div>
-                        
+          <!---->
+          <div v-if="this.b <= 0" class="cover31">
+
+            <div>
+              <img class="img30" src="../../assets/images/하트.png" alt="">
+            </div>
+            <div class="cover30">
+              <p>아직 찜한 업체가 없어요</p>
+              <p>관심있는 업체를 선택해보세요</p>
+            </div>
 
 
-                    </div>
-                    -->
-          <div class="wishlistCompany">
-            <div class="wishlistCompanyBox" v-for="(a, i) in a" :key="i">
+
+          </div>
+
+          <div v-else class="wishlistCompany">
+            <div class="wishlistCompanyBox" v-for="(a, i) in b" :key="i">
               <div>
                 <div class="whsilistImg wishlistCompanyImg">
-                  <img
-                    class=""
-                    src="../../assets/images/hs/main_slide_4.jpg"
-                    alt=""
-                    @click="goCompanyInfo"
-                  />
-                  <img
-                    v-on:click.prevent="modalUp"
-                    style="cursor: pointer"
+                  <img v-if="a.companyImage != null" class="" v-bind:src="require(`@/assets/images/company/${a.companyImage}.png`)" alt="" @click="goCompanyInfo" />
+                  <img v-else class="" src="../../assets/images/company/이미지없음.png" alt="" @click="goCompanyInfo" />
+                  <img v-on:click.prevent="modalUp" style="cursor: pointer"
                     src="data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cg clip-path='url(%23clip0_2517_457)'%3E %3Cpath d='M15.4448 27.0641L15.4368 27.0593L15.4064 27.0433C14.775 26.6998 14.1549 26.3359 13.5472 25.9521C12.0981 25.0404 10.7174 24.0243 9.41595 22.9121C6.47195 20.3729 3.19995 16.5633 3.19995 12.0001C3.20009 10.5112 3.66179 9.05903 4.52147 7.84344C5.38115 6.62785 6.59654 5.70866 8.00027 5.21244C9.40401 4.71622 10.9271 4.66737 12.3597 5.07263C13.7924 5.47788 15.0641 6.31729 16 7.4753C16.9358 6.31729 18.2075 5.47788 19.6402 5.07263C21.0728 4.66737 22.5959 4.71622 23.9996 5.21244C25.4034 5.70866 26.6187 6.62785 27.4784 7.84344C28.3381 9.05903 28.7998 10.5112 28.8 12.0001C28.8 16.5633 25.5296 20.3729 22.584 22.9121C20.7349 24.492 18.7274 25.8764 16.5936 27.0433L16.5632 27.0593L16.5552 27.0641H16.552C16.3819 27.1542 16.1925 27.2014 16.0001 27.2017C15.8078 27.2019 15.6182 27.1553 15.448 27.0657L15.4448 27.0641Z' fill='%23FD2748'/%3E %3C/g%3E %3Cdefs%3E %3CclipPath id='clip0_2517_457'%3E %3Crect width='32' height='32' fill='white'/%3E %3C/clipPath%3E %3C/defs%3E %3C/svg%3E"
-                    alt=""
-                  />
+                    alt="" />
                 </div>
                 <div class="wishTitleContent">
-                  <p class="">업체명업체명</p>
+                  <p class="">{{ a.classCount }}</p>
                   <div class="wihshCompanyContent">
                     <div>
                       <p class="">클래스</p>
@@ -108,7 +101,7 @@
           <p>삭제 하시겠습니까?</p>
         </div>
         <div class="modal-b">
-          <button>취소</button>
+          <button v-on:click="close">취소</button>
           <button>삭제</button>
         </div>
       </div>
@@ -117,6 +110,8 @@
   <AppFooter />
 </template>
 <script>
+import axios from 'axios';
+
 import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import mymyside from "@/components/MyPageSide.vue";
@@ -135,6 +130,7 @@ export default {
   data() {
     return {
       a: [1, 2, 3, 4, 5, 6],
+      b: []
     };
   },
 
@@ -155,8 +151,28 @@ export default {
       this.$router.push("/companyinfo");
     },
     ////////////////////////////////////////////////////////////////////////////////
+    listup() {
+      axios({
+        method: 'get', // put, post, delete 
+        url: `${this.$store.state.apiBaseUrl}/odo/wishcompany`,
+        headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": "Bearer " + this.$store.state.token }, //전송타입
+        //params: guestbookVo, //get방식 파라미터로 값이 전달
+        //data: this.$store.state.authUser.userNo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+        responseType: 'json' //수신타입
+      }).then(response => {
+        console.log(response); //수신데이타
+
+        console.log(response.data.apiData);
+        this.b = response.data.apiData
+        console.log(this.b);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
   },
-  created() {},
+  created() {
+    this.listup();
+  },
 };
 </script>
 
