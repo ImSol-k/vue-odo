af38fcd16c91359926571fe2c55a30a6943afbfd
m-ji97 <tv5035@naver.com>
2024-05-26 23:59:38 +0900

    mj

--- src/views/mj/ModifyView.vue
+++ src/views/mj/ModifyView.vue
@@ -14,6 +14,7 @@
         <div class="modify-head">
           <span>정보수정</span>
         </div>
+        <form v-on:submit.prevent="modify">
         <div class="MpRight" style="overflow: hidden">
           <img class="profile" src="@/assets/images/icon/header_icons/my.png" />
           <div id="p-box">
@@ -22,44 +23,45 @@
           <div class="area">
             <label>닉네임</label>
             <div>
-              <input type="text" id="nickname" v-model="userVo.name" />
+              <input type="text" id="nickname" name="usernickname" v-model="userVo.name" />
             </div>
           </div>
           <div class="area">
             <label>휴대폰번호</label>
             <div>
-              <input type="text" id="hp" v-model="userVo.hp" />
+              <input type="text" id="hp" name="userhp" v-model="userVo.hp" />
             </div>
           </div>
           <div class="area">
             <label>이메일</label>
             <div>
-              <input type="text" id="e-mail" v-model="userVo.email" />
+              <input type="text" id="e-mail" name="useremail" v-model="userVo.email" />
             </div>
           </div>
           <div class="area">
             <label>비밀번호</label>
             <div>
-              <input type="password" id="password" v-model="required" />
+              <input type="password" id="password" name="userpw" v-model="required" />
             </div>
           </div>
           <div class="area">
             <label>생년월일</label>
             <div>
-              <input type="text" id="birth" v-model="userVo.birth" />
+              <input type="text" id="birth" name="userbirth" disabled v-model="userVo.birth" />
             </div>
           </div>
           <div class="area">
             <label>성별</label>
             <div>
-              <input type="text" id="gender" disabled placeholder="여자" v-model="required" />
+              <input type="text" id="gender" name="usergender" disabled placeholder="여자" v-model="required" />
             </div>
           </div>
           <div class="userModifyBtn">
-            <button>취소</button>
+            <button type="reset">취소</button>
             <button>저장</button>
           </div>
         </div>
+</form>
       </div>
     </div>
   </div>
@@ -73,7 +75,7 @@ import AppFooter from "@/components/AppFooter.vue";
 import AppHeader from "@/components/AppHeader.vue";
 import MyPageSide from "@/components/MyPageSide.vue";
 import MyPageHeader from "@/components/MyPageHeader.vue";
-// import axios from 'axios';
+ import axios from 'axios';
 
 export default {
   name: "ModifyView",
@@ -86,7 +88,7 @@ export default {
   data() {
     return {
       userVo: {
-        name: "김소리",
+        name: "이미지",
         hp: "010-5443-5432",
         email: "sss@fds.fds",
         birth: "990523"
@@ -94,25 +96,25 @@ export default {
     };
   },
   methods: {
-    // axios({
-    //   method: 'get', // put, post, delete
-    //   //url: 'http://localhost:9000/mypage/modify',
-    //   url: `${this.$store.state.apiBaseUrl}/odo/modify`,
-    //   headers: { "Content-Type": "application/json; charset=utf-8","Autorization":"Bearer"+this.$store.state }, //전송타입
-    //   //params: guestbookVo, //get방식 파라미터로 값이 전달
-    //   data: this.userVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
-    //   responseType: 'json' //수신타입
-    // }).then(response => {
-    //     console.log(response/+.data.apiData);
-        
+    modify() {
+     axios({
+       method: 'post', // put, post, delete
+       url: `${this.$store.state.apiBaseUrl}/odo/mypage/modify`,
+       headers: { "Content-Type": "application/json; charset=utf-8","Autorization":"Bearer"+this.$store.state }, //전송타입
+       data: this.userVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+       responseType: 'json' //수신타입
+     }).then(response => {
+         console.log(response);
+        alert("수정되었습니다.");
 
-    //     //수신데이타
-    //   }).catch(error => {
-    //     console.log(error);
-    //   });
+         //수신데이타
+       }).catch(error => {
+         console.log(error);
+       });
+      }
   },
     created() {
-        this.modifyform();
+
     }
 };
 </script>
