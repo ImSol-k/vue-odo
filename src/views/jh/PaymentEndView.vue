0983c8efe68bb0dab68ca9ed2dca26fc4ebfd688
hyeon0714 <xodid6960@naver.com>
2024-05-28 16:59:27 +0900

    gg

--- src/views/jh/PaymentEndView.vue
+++ src/views/jh/PaymentEndView.vue
@@ -3,35 +3,36 @@
   <div class="wrap">
     <div class="paymentEndBox">
       <h1>결제가 완료되었습니다</h1>
-      <h2>예약자: <span>김소리</span></h2>
+      <h2>예약자: <span>{{pv.nickname}}</span></h2>
       <table class="pamentTable">
         <tbody>
           <tr>
             <td rowspan="7">
-              <img src="../../assets/images/hs/main_slide_3.jpg" alt="" />
+              <img v-if="pv.classImage != ''" :src="`${this.$store.state.apiBaseUrl}/upload/${pv.classImage}`" alt="" />
             </td>
             <th>업체명</th>
-            <td>삼송</td>
+            <td>{{ pv.companyName }}</td>
           </tr>
           <tr>
             <th>클래스명</th>
-            <td>반도체 체험</td>
+            <td>{{pv.className}}</td>
           </tr>
           <tr>
             <th>일시</th>
-            <td>2024-05-13 10:00</td>
+            <td v-if="pv.endDate == null">{{pv.startDate}}</td>
+            <td v-else>{{pv.startDate}} ~ {{ pv.endDate }}</td>
           </tr>
           <tr>
             <th>결제방법</th>
-            <td colspan="2">네이버페이</td>
+            <td colspan="2">{{pv.payType}}</td>
           </tr>
           <tr>
             <th>할인금액</th>
-            <td colspan="2">{{ Number(5000).toLocaleString("ko-KR") }}</td>
+            <td colspan="2">{{ Number(pv.couponPrice).toLocaleString("ko-KR") }} 원</td>
           </tr>
           <tr>
             <th>총결제금액</th>
-            <td colspan="2">{{ Number(100000).toLocaleString("ko-KR") }}</td>
+            <td colspan="2">{{ Number(pv.payPirce).toLocaleString("ko-KR") }} 원</td>
           </tr>
         </tbody>
       </table>
@@ -45,16 +46,16 @@
         <h3>이런 클래스도 있어요</h3>
       </div>
       <div class="paymentClassBox">
-        <div class="paymentClass" v-for="i in 5" :key="i">
-          <img src="../../assets/images/hs/main_slide_2.jpg" alt="" />
-          <p>지역</p>
-          <p>일일 자전거 클래스</p>
+        <div class="paymentClass" v-for="i in rList" :key="i">
+          <img v-bind:src="`${this.$store.state.apiBaseUrl}/upload/${i.classImage}`" alt="" />
+          <p>{{ i.classAddress }}</p>
+          <p>{{ i.className }}</p>
           <p>
-            설명설명설명설명설명설명..
+            {{ i.classIntro }}
           </p>
           <div class="paymentClassStar">
             <span class="paymentStar" v-for="(i) in 5" :key="i">★</span>
-            <span>후기 123</span>
+            <span>후기 {{i.reviewCount}}</span>
           </div>
           <p>무료</p>
         </div>
@@ -64,6 +65,8 @@
   <AppFooter />
 </template>
 <script>
+import axios from 'axios';
+
 import AppHeader from "@/components/AppHeader.vue";
 import AppFooter from "@/components/AppFooter.vue";
 import "@/assets/css/jh/jh.css";
@@ -78,10 +81,44 @@ export default {
   data() {
     return {
       a: 4,
+      pv: {
+        classImage: '',
+        className: '',
+        companyName: '',
+        couponPrice: 0,
+        endDate: '',
+        startDate: '',
+        nickname: '',
+        payPirce: 0,
+        payType: ''
+      },
+      rList: []
     };
   },
-  methods: {},
-  created() {},
+  methods: {
+    list(){
+      console.log("시작");
+      axios({
+        method: 'get', // put, post, delete 
+        url: `${this.$store.state.apiBaseUrl}/odo/payend/${this.$route.params.no}`,
+        headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": "Bearer " + this.$store.state.token }, //전송타입
+        //params: guestbookVo, //get방식 파라미터로 값이 전달
+        //data: this.$store.state.authUser.userNo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+        responseType: 'json' //수신타입
+      }).then(response => {
+        console.log(response); //수신데이타
+        this.pv = response.data.apiData.pv;
+        this.rList = response.data.apiData.rList;
+        console.log(this.pv);
+        console.log(this.rList);
+      }).catch(error => {
+        console.log(error);
+      });
+    }
+  },
+  created() {
+    this.list();
+  },
 };
 </script>
 
