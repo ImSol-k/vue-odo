b6b6b7505e3fbb6235dbe577eec2e0df64813ca9
rhs0402 <rhs0402@naver.com>
2024-05-29 18:29:16 +0900

    하석-사이드메뉴 토글 수정

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -82,9 +82,12 @@
             <div class="inner">
                 <router-link to="/companyselect">호스트 지원</router-link>
                 <ul>
-                    <li v-if="(this.$store.state.authUser == '' && this.$store.state.token == '')"><router-link to="/join">회원가입</router-link></li>
-                    <li v-else><router-link to="/mypage/pay" @click="goMypage">{{ this.$store.state.authUser.userNickname }}님</router-link></li>
-                    <li v-if="(this.$store.state.authUser == '' || this.$store.state.token == '')"><router-link to="/login/user">로그인</router-link></li>
+                    <li v-if="(this.$store.state.authUser == '' && this.$store.state.token == '')"><router-link
+                            to="/join">회원가입</router-link></li>
+                    <li v-else><router-link to="/mypage/pay" @click="goMypage">{{
+        this.$store.state.authUser.userNickname }}님</router-link></li>
+                    <li v-if="(this.$store.state.authUser == '' || this.$store.state.token == '')"><router-link
+                            to="/login/user">로그인</router-link></li>
                     <li v-else><router-link to="/" @click="logout">로그아웃</router-link></li>
                     <li><router-link to="">자주 묻는 질문</router-link></li>
                     <li><router-link to="/mypage/notice">공지사항</router-link></li>
@@ -120,9 +123,9 @@ export default {
     },
     data() {
         return {
-            subCateNo: [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29],
+            subCateNo: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29],
             isMenuOpen: false,
-            isSubMenuOpen:  {
+            isSubMenuOpen: {
                 1: false,
                 2: false,
                 3: false,
@@ -137,18 +140,18 @@ export default {
     methods: {
         /////////////////////////////// ss /////////////////////////////////////
         // 로그아웃
-        logout(){
+        logout() {
             this.$store.commit('setAuthUser', '');
             this.$store.commit('setToken', '');
             this.$router.push('/login/user');
         },
         // 로그인 체크
-        checkAuth(){
-            if((this.$store.state.authUser == '' && this.$store.state.token == '')){
+        checkAuth() {
+            if ((this.$store.state.authUser == '' && this.$store.state.token == '')) {
                 this.$router.push('/login/user');
             }
         },
-        goMypage(){
+        goMypage() {
             this.$router.push('/mypage/pay');
         },
         /////////////////////////////////////////////////////////////////////////
@@ -156,29 +159,64 @@ export default {
         ///////////////////////////////////// 사이드 바 ///////////////////////////////
         toggleMenu() {
             this.isMenuOpen = !this.isMenuOpen;
-            for(let i=1; i<8; i++) {
+            for (let i = 1; i < 8; i++) {
                 this.isSubMenuOpen[i] = false;
             }
         },
         toggleSubMenu1() {
-            this.isSubMenuOpen[1] = !this.isSubMenuOpen[1];
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '1') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
+            this.isSubMenuOpen[1] = !this.isSubMenuOpen[1];  // '1'은 그대로 토글
         },
         toggleSubMenu2() {
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '2') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
             this.isSubMenuOpen[2] = !this.isSubMenuOpen[2];
         },
         toggleSubMenu3() {
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '3') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
             this.isSubMenuOpen[3] = !this.isSubMenuOpen[3];
         },
         toggleSubMenu4() {
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '4') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
             this.isSubMenuOpen[4] = !this.isSubMenuOpen[4];
         },
         toggleSubMenu5() {
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '5') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
             this.isSubMenuOpen[5] = !this.isSubMenuOpen[5];
         },
         toggleSubMenu6() {
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '6') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
             this.isSubMenuOpen[6] = !this.isSubMenuOpen[6];
         },
         toggleSubMenu7() {
+            Object.keys(this.isSubMenuOpen).forEach(key => {
+                if (key !== '7') {  // '1'을 제외하고 false로 설정
+                    this.isSubMenuOpen[key] = false;
+                }
+            });
             this.isSubMenuOpen[7] = !this.isSubMenuOpen[7];
         },
         cateMouseover() {
@@ -190,7 +228,7 @@ export default {
         ///////////////////////////////////////////////////////////////////////////////////////////////
     },
     created() {
-    
+
     }
 };
 </script>
