6d393d9fbf2fc9783710de4c3aaee0dbe514a84b
hyeon0714 <xodid6960@naver.com>
2024-05-31 12:00:02 +0900

    .3..

--- src/views/jh/WishListCompany.vue
+++ src/views/jh/WishListCompany.vue
@@ -2,7 +2,7 @@
   <!--업체 소개페이지-->
   <AppHeader />
   <div class="wrap">
-
+    
     <MyPageHeader />
     <!-- <div class="mymy-header clearfix">
       <img
@@ -24,7 +24,7 @@
     <div class="mymy clearfix">
       <mymyside />
       <!-- mypage-side -->
-
+      
       <div class="mymy-content">
         <div class="mymy-head">
           <span>위시리스트</span>
@@ -46,10 +46,13 @@
             <div class="wishlistCompanyBox" v-for="(a, i) in b" :key="i">
               <div>
                 <div class="whsilistImg wishlistCompanyImg">
-                  <img v-if="a.companyImage != null" class="" v-bind:src="`${this.$store.state.apiBaseUrl}/upload/${a.companyImage}`" alt="" @click="goCompanyInfo" />
-                  <img v-else class="" v-bind:src="`${this.$store.state.apiBaseUrl}/upload/noneImage.jpg`" alt="" @click="goCompanyInfo" />
+                  <img v-if="a.companyImage != null" class=""
+                    v-bind:src="`${this.$store.state.apiBaseUrl}/upload/${a.companyImage}`" alt=""
+                    @click="goCompanyInfo(a.companyNo)" />
+                  <img v-else class="" v-bind:src="`${this.$store.state.apiBaseUrl}/upload/noneImage.jpg`" alt=""
+                    @click="goCompanyInfo(a.companyNo)" />
                   <img v-on:click.prevent="modalUp(a.no)" style="cursor: pointer"
-                    src="data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cg clip-path='url(%23clip0_2517_457)'%3E %3Cpath d='M15.4448 27.0641L15.4368 27.0593L15.4064 27.0433C14.775 26.6998 14.1549 26.3359 13.5472 25.9521C12.0981 25.0404 10.7174 24.0243 9.41595 22.9121C6.47195 20.3729 3.19995 16.5633 3.19995 12.0001C3.20009 10.5112 3.66179 9.05903 4.52147 7.84344C5.38115 6.62785 6.59654 5.70866 8.00027 5.21244C9.40401 4.71622 10.9271 4.66737 12.3597 5.07263C13.7924 5.47788 15.0641 6.31729 16 7.4753C16.9358 6.31729 18.2075 5.47788 19.6402 5.07263C21.0728 4.66737 22.5959 4.71622 23.9996 5.21244C25.4034 5.70866 26.6187 6.62785 27.4784 7.84344C28.3381 9.05903 28.7998 10.5112 28.8 12.0001C28.8 16.5633 25.5296 20.3729 22.584 22.9121C20.7349 24.492 18.7274 25.8764 16.5936 27.0433L16.5632 27.0593L16.5552 27.0641H16.552C16.3819 27.1542 16.1925 27.2014 16.0001 27.2017C15.8078 27.2019 15.6182 27.1553 15.448 27.0657L15.4448 27.0641Z' fill='%23FD2748'/%3E %3C/g%3E %3Cdefs%3E %3CclipPath id='clip0_2517_457'%3E %3Crect width='32' height='32' fill='white'/%3E %3C/clipPath%3E %3C/defs%3E %3C/svg%3E"
+                    :src="img"
                     alt="" />
                 </div>
                 <div class="wishTitleContent">
@@ -88,11 +91,12 @@
 
           </div>
 
-          
+
         </div>
         <!-- mymy-main -->
       </div>
       <!-- mypage-content -->
+
     </div>
 
     <div id="newModal">
@@ -133,7 +137,8 @@ export default {
     return {
       a: [1, 2, 3, 4, 5, 6],
       b: [],
-      c:''
+      c: '',
+      img: "data:image/svg+xml,%3Csvg width='32' height='32' viewBox='0 0 32 32' fill='none' xmlns='http://www.w3.org/2000/svg'%3E %3Cg clip-path='url(%23clip0_2517_457)'%3E %3Cpath d='M15.4448 27.0641L15.4368 27.0593L15.4064 27.0433C14.775 26.6998 14.1549 26.3359 13.5472 25.9521C12.0981 25.0404 10.7174 24.0243 9.41595 22.9121C6.47195 20.3729 3.19995 16.5633 3.19995 12.0001C3.20009 10.5112 3.66179 9.05903 4.52147 7.84344C5.38115 6.62785 6.59654 5.70866 8.00027 5.21244C9.40401 4.71622 10.9271 4.66737 12.3597 5.07263C13.7924 5.47788 15.0641 6.31729 16 7.4753C16.9358 6.31729 18.2075 5.47788 19.6402 5.07263C21.0728 4.66737 22.5959 4.71622 23.9996 5.21244C25.4034 5.70866 26.6187 6.62785 27.4784 7.84344C28.3381 9.05903 28.7998 10.5112 28.8 12.0001C28.8 16.5633 25.5296 20.3729 22.584 22.9121C20.7349 24.492 18.7274 25.8764 16.5936 27.0433L16.5632 27.0593L16.5552 27.0641H16.552C16.3819 27.1542 16.1925 27.2014 16.0001 27.2017C15.8078 27.2019 15.6182 27.1553 15.448 27.0657L15.4448 27.0641Z' fill='%23FD2748'/%3E %3C/g%3E %3Cdefs%3E %3CclipPath id='clip0_2517_457'%3E %3Crect width='32' height='32' fill='white'/%3E %3C/clipPath%3E %3C/defs%3E %3C/svg%3E"
     };
   },
 
@@ -153,8 +158,8 @@ export default {
       viewModal.style.display = "none";
     },
     ///////////////////////////// 이미지 누르면 업체상세페이지로 이동 //////////////////////
-    goCompanyInfo() {
-      this.$router.push("/companyinfo");
+    goCompanyInfo(no) {
+      this.$router.push("/companyinfo/" + no);
     },
     ////////////////////////////////////////////////////////////////////////////////
     listup() {
@@ -175,7 +180,7 @@ export default {
         console.log(error);
       });
     },
-    del(){
+    del() {
       axios({
         method: 'delete', // put, post, delete 
         url: `${this.$store.state.apiBaseUrl}/odo/wishcompany`,
