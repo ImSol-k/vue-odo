aa1d29fb6d3b3b84b0562bc7c9e0d185eaf42d8c
m-ji97 <tv5035@naver.com>
2024-06-03 18:46:27 +0900

    mj

--- src/views/mj/ChartView.vue
+++ src/views/mj/ChartView.vue
@@ -12,35 +12,29 @@
           </div>
         </div>
         <div class="list clearfix">
-          <div class="whatClass"> 
-            <span
-              v-on:click.prevent="selectClass(1)"
-              :class="{ classChack: isClass == 1 }"
-              >원데이</span>
-            <span
-              v-on:click.prevent="selectClass(2)"
-              :class="{ classChack: isClass == 2 }"
-              >정규/상시</span>
+          <div class="whatClass">
+            <span v-on:click.prevent="selectClass(1)" :class="{ classChack: isClass == 1 }">원데이</span>
+            <span v-on:click.prevent="selectClass(2)" :class="{ classChack: isClass == 2 }">정규/상시</span>
           </div>
-          
+
           <div v-if="isClass == 1">
             <div class="right">
-                <img src="@/assets/images/icon/header_icons/like.png">
-                <span>인기가 많은 클래스</span>
+              <img src="@/assets/images/icon/header_icons/like.png">
+              <span>인기가 많은 클래스</span>
             </div>
           </div>
 
           <div v-else-if="isClass == 2">
-              <div class="right">
-                <img src="@/assets/images/icon/header_icons/like.png">
-                <span>정규 인기가 많은 클래스</span>
+            <div class="right">
+              <img src="@/assets/images/icon/header_icons/like.png">
+              <span>정규 인기가 많은 클래스</span>
             </div>
-            
+
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
   <AppFooter />
 </template>
 
@@ -51,6 +45,7 @@ import AppMenu from "@/components/CompanyMenu.vue";
 import "@/assets/css/mj/chart.css";
 import { Chart, registerables } from 'chart.js';
 Chart.register(...registerables);
+import axios from 'axios';
 
 export default {
   name: "ModifyView",
@@ -62,58 +57,102 @@ export default {
   data() {
     return {
       isClass: 1,
+      chart: {
+        onedayPrice: "",
+        preRePrice: "",
+        preonedayPrice: "",
+        rePrice: ""
+      },
+      list: [],
+      list1: []
     };
   },
   methods: {
     selectClass(num) {
       if (num == 1) {
         this.isClass = 1;
-      } else if(num == 2) {
+      } else if (num == 2) {
         this.isClass = 2;
       }
-    }
+    },
+    map() {
+      axios({
+        method: 'get', // put, post, delete                   
+        url: `${this.$store.state.apiBaseUrl}/odo/chart`,
+        headers: { "Content-Type": "application/json; charset=utf-8", "Authorization": "Bearer " + this.$store.state.token }, //전송타입
+        //params: guestbookVo, //get방식 파라미터로 값이 전달
+        //data: guestbookVo, //put, post, delete 방식 자동으로 JSON으로 변환 전달
+
+        responseType: 'json' //수신타입
+      }).then(response => {
+        console.log(response.data.chart); //수신데이타
+        this.chart = response.data.chart;
+
+        this.list = response.data.list;
+        this.list1 = response.data.list1;
+
+        console.log(this.chart);
+        console.log(this.list);
+        console.log(this.list1);
+
+        new Chart(this.$refs.MyChart, {
+
+          type: 'doughnut',
+          data: {
+            labels: ['지난달', '이번달', '총테이블', '총옷장'],
+            datasets: [{
+              dataIndex: true,
+              display: true,
+              data: [this.allbed, this.allshopa, this.alltable, this.allhanger],
+              datalabels: {
+                color: '#000000'
+              },
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+
+              ],
+              borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+
+              ],
+              borderWidth: 1
+            }],
+
+          },
+
+          options: {
+            responsive: false,
+            plugins: {
+              datalabels: {
+                color: '#000000'
+              },
+              title: {
+                display: true,
+                text: '총 판매비율'
+              },
+            }
+          }
+        })
+
+      }).catch(error => {
+        console.log(error);
+
+      });
+    },
+
   },
-	created(){},
-  mounted() {
-      const labels = [
-      '24년 1월',
-      '2월',
-      '3월',
-      '4월',
-      '5월',
-      '24년 6월',
-    ];
-    const data = {
-      labels: labels,
-      datasets: [{
-        label: '원데이',
-        data: [30, 35, 45, 50, 60, 75],
-        fill: false,
-        borderColor: 'rgb(75, 192, 192)',
-        tension: 0.2
-      },
-      {
-        label: '정규/상시',
-        data:[30, 45, 50, 65, 70, 80],
-        fill:false,
-        borderColor: 'rgb(30, 200, 120)',
-        tension: 0.2
-      }
-    ],
-    };
-    const config = {
-      type: 'line',
-      data: data,
-    };
-    new Chart(
-      document.getElementById('LineChart'),
-      config
-    )
-  }
-  
+  created() {
+    this.map();
+  },
+
+
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
