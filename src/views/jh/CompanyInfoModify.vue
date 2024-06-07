42f5580104dd98fcdc3d16db349d1e3820009003
hyeon0714 <xodid6960@naver.com>
2024-06-07 19:16:59 +0900

    ㅎㅎ

--- src/views/jh/CompanyInfoModify.vue
+++ src/views/jh/CompanyInfoModify.vue
@@ -26,34 +26,18 @@
       <h1 class="companyModifyName">{{ this.companyVo.companyName }}</h1>
       <div class="companyJoinImg">
         <div class="preview-image">
-          <img
-            v-bind:src="previewImage"
-            style="width: 180px; height: 180px; border-radius: 10px"
-          />
+          <img v-bind:src="previewImage" style="width: 180px; height: 180px; border-radius: 10px" />
         </div>
 
         <div class="input">
-          <input
-            type="file"
-            id="profile"
-            name="profile"
-            class="file-input"
-            style="width: auto"
-            @change="handleImageChange"
-          />
+          <input type="file" id="profile" name="profile" class="file-input" style="width: auto"
+            @change="handleImageChange" />
         </div>
       </div>
       <div class="companyJoinInfo">
         <div>
           <!-- <label for="companyNo">사업자번호</label> -->
-          <input
-            type="text"
-            name=""
-            id="businesNumber"
-            v-model="companyVo.companyBn"
-            placeholder="사업자번호"
-            disabled
-          />
+          <input type="text" name="" id="businesNumber" v-model="companyVo.companyBn" placeholder="사업자번호" disabled />
           <!-- <input type="button" value="사업자 인증" v-on:click="businessNumber" /> -->
           <!-- <p v-if="isBn">✅인증되었습니다</p>
           <p v-else-if="isBn == null">사업자번호를 입력해주세요.</p>
@@ -61,111 +45,45 @@
         </div>
         <div>
           <!-- <label for="companyName">업체명</label> -->
-          <input
-            type="text"
-            name=""
-            id="companyName"
-            v-model="companyVo.companyName"
-            placeholder="업체명"
-          />
+          <input type="text" name="" id="companyName" v-model="companyVo.companyName" placeholder="업체명" />
         </div>
         <div>
           <!-- <label for="companyName">업체명</label> -->
-          <textarea
-            type="text"
-            id="companyDescription"
-            v-model="companyVo.companyIntro"
-            placeholder="업체소개"
-          >
+          <textarea type="text" id="companyDescription" v-model="companyVo.companyIntro" placeholder="업체소개">
           </textarea>
         </div>
         <div>
           <!-- <label for="companyId">아이디</label> -->
-          <input
-            type="text"
-            name=""
-            id="companyId"
-            v-model="companyVo.companyId"
-            placeholder="아이디"
-            disabled
-          />
+          <input type="text" name="" id="companyId" v-model="companyVo.companyId" placeholder="아이디" disabled />
         </div>
         <div>
           <!-- <label for="companyPass">비밀번호</label> -->
-          <input
-            type="password"
-            name=""
-            id="companyPass"
-            placeholder="비밀번호 변경"
-            v-model="companyPassChack"
-            minlength="8"
-            maxlength="16"
-          />
+          <input type="password" name="" id="companyPass" placeholder="비밀번호 변경" v-model="companyPassChack" minlength="8"
+            maxlength="16" />
           <p>비밀번호를 입력해주세요.(8~16글자)</p>
         </div>
         <div>
           <!-- <label for="companyPassChack">비밀번호확인</label> -->
-          <input
-            type="password"
-            name=""
-            id="companyPassChack"
-            placeholder="비밀번호 확인"
-            v-model="companyVo.companyPass"
-          />
-          <p
-            v-if="
-              companyPassChack != '' &&
-              companyPassChack == companyVo.companyPass
-            "
-          >
+          <input type="password" name="" id="companyPassChack" placeholder="비밀번호 확인" v-model="companyVo.companyPass" />
+          <p v-if="companyPassChack != '' &&
+        companyPassChack == companyVo.companyPass
+        ">
             ✅비밀번호 일치
           </p>
           <p v-else>❌비밀번호가 일치하지 않습니다.</p>
         </div>
         <div class="companyAddress">
-          <input
-            type="text"
-            id="companyZipCode"
-            placeholder="우편번호"
-            v-model="companyVo.companyZipCode"
-            readonly
-          />
-          <input
-            type="button"
-            v-on:click.prevent="DaumPostcode()"
-            value="우편번호 찾기"
-          /><br />
-          <input
-            type="text"
-            id="companyNameAddress"
-            placeholder="도로명주소"
-            v-model="companyVo.companyNameAddress"
-            readonly
-          />
-          <input
-            type="text"
-            id="companyNumAddress"
-            placeholder="지번주소"
-            v-model="companyVo.companyNumAddress"
-            readonly
-          />
+          <input type="text" id="companyZipCode" placeholder="우편번호" v-model="companyVo.companyZipCode" readonly />
+          <input type="button" v-on:click.prevent="DaumPostcode()" value="우편번호 찾기" /><br />
+          <input type="text" id="companyNameAddress" placeholder="도로명주소" v-model="companyVo.companyNameAddress"
+            readonly />
+          <input type="text" id="companyNumAddress" placeholder="지번주소" v-model="companyVo.companyNumAddress" readonly />
           <span id="guide" style="color: #999; display: none"></span>
-          <input
-            type="text"
-            id="companyDetailAddress"
-            placeholder="상세주소"
-            v-model="companyVo.companyDetailAddress"
-          />
+          <input type="text" id="companyDetailAddress" placeholder="상세주소" v-model="companyVo.companyDetailAddress" />
         </div>
         <div>
           <!-- <label for="companyPassChack">비밀번호확인</label> -->
-          <input
-            type="text"
-            name=""
-            id="companyPassChack"
-            placeholder="대표 핸드폰번호"
-            v-model="companyVo.companyHp"
-          />
+          <input type="text" name="" id="companyPassChack2" placeholder="대표 핸드폰번호" v-model="companyVo.companyHp" />
         </div>
       </div>
       <div class="companyJoinButton">
@@ -187,6 +105,7 @@ import AppFooter from "@/components/AppFooter.vue";
 import AppMenu from "@/components/CompanyMenu.vue";
 import "@/assets/css/jh/jh.css";
 import "@/assets/css/Initialization.css";
+import Swal from 'sweetalert2';
 
 import axios from "axios";
 
@@ -289,8 +208,12 @@ export default {
           /* eslint-disable */
           .then((response) => {
             if (response.data.result == "success") {
-              alert("수정되었습니다.");
-              this.$router.push("/companypage");
+              Swal.fire({
+                title: "수정되었습니다",
+              }).then(function () {
+                location.href = '/companypage';
+                //this.$router.push("/companypage");
+              });
             }
           })
           .catch((error) => {
